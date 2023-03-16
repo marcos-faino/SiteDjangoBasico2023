6662068417605a43bfc0414507fdade7cf07cd2a
Marcos Faino <marcos.faino@ifro.edu.br>
2023-03-15 20:30:03 -0400

    Corrigido problema com imagens

--- core/views.py
+++ core/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from core.models import Funcionario, Servico
 
 
 class IndexView(TemplateView):
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        contexto = super().get_context_data(**kwargs)
+        # context = super(IndexView, self).get_context_data(**kwargs)
+        contexto['funcionarios'] = Funcionario.objects.all()
+        contexto['servicos'] = Servico.objects.all()
+        return contexto
+
